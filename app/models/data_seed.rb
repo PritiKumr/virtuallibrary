fd932a115ae8d5d0d467834a0dc6a3b63b928e48
sur08 <ignnovator@gmail.com>
2016-09-12 10:50:57 +0530

    Added data seed for books.

--- app/models/data_seed.rb
+++ app/models/data_seed.rb
@@ -1,7 +1,9 @@
 class DataSeed < ApplicationRecord
-  Author.create(name: "Ashwin Sanghi")
-  Publication.create(name: "Red Hot Chillies")
-  Book.create(sku: "AB123K", name: "The krishna key", price: 120, author_id: 1, publication_id: 1)
+  def self.books
+    Author.create(name: "Ashwin Sanghi")
+    Publication.create(name: "Red Hot Chillies")
+    Book.create(sku: "AB123K", name: "The krishna key", price: 120, author_id: 1, publication_id: 1)
+  end
 
   def self.plans
     plan1  = Plan.create(name: 'One Month')
