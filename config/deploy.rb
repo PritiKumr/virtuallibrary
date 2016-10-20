c91936bfd9318e3f6b208efde71990cffa58332e
sur08 <ignnovator@gmail.com>
2016-10-21 01:11:03 +0530

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 set :application, 'virtuallibrary'
 set :repo_url, 'git@github.com:PritiKumr/virtuallibrary.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/virtuallibrary'
+set :deploy_to, '/home/kitabiworm/virtuallibrary'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
